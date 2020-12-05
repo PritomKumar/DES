4c6d174edbb25f965accdaab87c703636ea92a0e
PritomKumar <bsse0919@iit.du.ac.bd>
2020-12-06 00:44:14 +0600

    decimalToBinary convert

--- DES.cpp
+++ DES.cpp
@@ -275,21 +275,19 @@ string XORstep(string r0AfterEBItExpansion, string subKey)
 int binaryToDecimal(int n)
 {
     int num = n;
-    int dec_value = 0;
+    int decValue = 0;
     int base = 1;
 
     int temp = num;
     while (temp)
     {
-        int last_digit = temp % 10;
+        int lastDigit = temp % 10;
         temp = temp / 10;
-
-        dec_value += last_digit * base;
-
+        decValue += lastDigit * base;
         base = base * 2;
     }
 
-    return dec_value;
+    return decValue;
 }
 
 int stringToInt(string str)
