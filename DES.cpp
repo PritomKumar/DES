67cb431dccaf75700548350d6c1c69bb0f31f690
PritomKumar <bsse0919@iit.du.ac.bd>
2020-12-06 16:48:42 +0600

    final permutation done

--- DES.cpp
+++ DES.cpp
@@ -473,7 +473,7 @@ string secondPhase(){
         //cout << l0 << endl;
         //cout << r1 << endl;
     }
-
+/*
     for(int j=0;j<17;j++){
         cout << "Round " << j <<endl;;
         cout<<"Left Part   "<<endl;
@@ -493,9 +493,30 @@ string secondPhase(){
         }
         cout<<endl;
     }
-
+*/
 }
 
+
+string finalPermutation(string resultBeforePermutation)
+{
+    int FinalPermutation[64] = {
+            40,     8,   48,    16,    56,   24,    64,   32,
+            39,     7,   47,    15,    55,   23,    63,   31,
+            38,     6,   46,    14,    54,   22,    62,   30,
+            37,     5,   45,    13,    53,   21,    61,   29,
+            36,     4,   44,    12,    52,   20,    60,   28,
+            35,     3,   43,    11,    51,   19,    59,   27,
+            34,     2,   42,    10,    50,   18,    58,   26,
+            33,     1,   41,     9,    49,   17,    57,   25,
+        };
+
+    string finalResult = "";
+    for (int i = 0; i < 64; i++)
+    {
+        finalResult += resultBeforePermutation[FinalPermutation[i] - 1];
+    }
+    return finalResult;
+}
 int main()
 {
     string plainText = "PRITOMKD";
@@ -523,5 +544,9 @@ int main()
     rightPartList[0] = r0;
 
     secondPhase();
+
+    string enCryptedBinary = finalPermutation(rightPartList[16]+leftPartList[16]);
+
+    cout << enCryptedBinary <<endl;
 
 }
